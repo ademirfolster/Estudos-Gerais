ef756a2983683c5aa9d9f16adec1b0cb30bf002d
Ademir Folster <ademirfolster2@gmail.com>
2026-04-18 09:46:45 -0300

    style: convertido para tailwind css playButton.tsx

--- react-1/src/components/playButton.tsx
+++ react-1/src/components/playButton.tsx
@@ -7,17 +7,7 @@ export function PlayButton({ label, onClick }: playButtonProps) {
   return (
     <button
       onClick={onClick}
-      style={{
-        padding: "15px 30px",
-        fontSize: "1.2rem",
-        backgroundColor: "#BA8E23",
-        color: "black",
-        border: "2px solid black",
-        borderRadius: "12px",
-        cursor: "pointer",
-        fontWeight: "bold",
-        textTransform: "uppercase",
-      }}
+      className="py-4 px-8 text-xl bg-yellow-600 text-black border-2 border-black rounded-xl cursor-pointer font-bold uppercase"
     >
       {label}
     </button>
