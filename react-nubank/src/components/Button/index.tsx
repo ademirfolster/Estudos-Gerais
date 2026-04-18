2fa3aee74fb99188855c1bf10abf28fde16c675a
Ademir Folster <ademirfolster2@gmail.com>
2026-04-17 21:36:29 -0300

    fix: correção de pastas gerais

--- react-nubank/src/components/Button/index.tsx
+++
@@ -1,6 +0,0 @@
-interface ButtonProps {
-  label: string;
-  variant?: "primary" | "secondary" | "cta";
-  onClick?: () => void;
-}
-export function Button({ label, variant = "primary", onClick }: ButtonProps) {}
