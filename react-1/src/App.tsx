d5cd9fa6efda3451b31cc39d96725ffc436ce330
Ademir Folster <ademirfolster2@gmail.com>
2026-04-18 08:55:14 -0300

    feat: adicionado tailwindcss e convertido app.tsx

--- react-1/src/App.tsx
+++ react-1/src/App.tsx
@@ -1,24 +1,15 @@
 import { useState } from "react";
 import { PlayButton } from "./components/playButton";
-import  Game  from "./game/Game";
-
+import Game from "./game/Game";
 
 export default function App() {
   const [jogar, setJogar] = useState(false);
 
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        gap: "30px",
-        height: "100vh",
-        maxHeight: "100%",
-      }}
-    >
-      {!jogar ? <PlayButton label="Jogar" onClick={() => setJogar(!jogar)} /> : null}
+    <div className="flex flex-col items-center justify-center gap-8 h-screen max-h-full">
+      {!jogar ? (
+        <PlayButton label="Jogar" onClick={() => setJogar(!jogar)} />
+      ) : null}
       {jogar ? <Game jogar={setJogar} /> : undefined}
     </div>
   );
